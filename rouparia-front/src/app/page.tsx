67d7515aece9cf2a875b055ae95033c834965bb3
Roberto-Ceschini <robert17ceschini@gmail.com>
2025-03-12 13:15:37 -0300

    Customização de fontes

--- rouparia-front/src/app/page.tsx
+++ rouparia-front/src/app/page.tsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex w-[10vw] h-[300vh] border-2 bg-azul-primario
+    <div className="flex w-[100vw] h-[100vh] border-2 bg-verde-primario justify-center items-center
     ">
-      
+     <div className="flex justify-center items-center p-2 w-[80%] h-[50%] bg-verde-terciario rounded-xl">
+      <h1 className="font-bold">Login</h1>
+      <></>
+     </div>
     </div>
   );
 }
