07c5d077ed55c5e927f4dfb23a96ee8dbe1557f2
Roberto-Ceschini <robert17ceschini@gmail.com>
2025-04-21 02:58:05 -0300

    Suspense error

--- rouparia-front/src/app/cadastrarAreaVinculo/page.tsx
+++ rouparia-front/src/app/cadastrarAreaVinculo/page.tsx
@@ -1,102 +1,11 @@
-"use client";
-import HeaderHistoricoColaborador from "@/components/HeaderHistoricoColaborador";
-import api from "@/services/axios";
-import { useEffect, useState } from "react";
-import TabelaAreas from "@/components/TabelaArea";
-import { Area } from "@/types/area";
-import { useSearchParams } from "next/navigation";
-import { Search } from "lucide-react";
+// app/cadastrarAreaVinculo/page.tsx
+import { Suspense } from "react";
+import CadastrarAreaVinculo from "./cadastrarAreaVinculo";
 
-export default function CadastrarAreaVinculo() {
-  //------------VARIAVEIS----------
-  const [areasOriginais, setAreasOriginais] = useState<[] | Area[]>([]); // Áreas que serão exibidas no dropDown
-  const [areasFiltradas, setAreasFiltradas] = useState<[] | Area[]>([]);
-
-  const useParams = useSearchParams();
-  const tipo = useParams.get('tipo');
-
-  const [textoBusca, setTextoBusca] = useState("");
-
-  const pesquisar = (texto: string) => {
-    const textoFormatado = texto.toLowerCase();
-
-    if (textoFormatado.trim() === "") {
-      setAreasFiltradas(areasOriginais); // Aqui você deve ter salvo as áreas originais em algum lugar
-      return;
-    }
-
-    const filtradas = areasOriginais.filter((a) =>
-      a.nome.toLowerCase().includes(textoFormatado)
-    );
-
-    setAreasFiltradas(filtradas);
-  };
-
-
-  const fetchAreas = async () => {
-    try {
-      const response = await api.get("/area");
-      if (response.data) return response.data;
-    } catch (error) {
-      alert(`Erro ao carregar áreas, recarregue a página\n${error}`);
-    }
-  }
-
-  const fetchVinculos = async () => {
-    try {
-      const response = await api.get("/vinculo");
-      if (response.data) return response.data;
-    }
-    catch (error) {
-      alert(`Erro ao carregar vínculos, recarregue a página\n${error}`);
-    }
-  }
-
-  useEffect(() => {
-    const carregarDados = async () => {
-      if (tipo === 'area') {
-        const areasData = await fetchAreas();
-        if (areasData) {
-          setAreasOriginais(areasData)
-          setAreasFiltradas(areasData);
-        };
-      }
-      else if (tipo === 'vinculo') {
-        const areasData = await fetchVinculos();
-        if (areasData) {
-          setAreasOriginais(areasData)
-          setAreasFiltradas(areasData);
-        };
-      };
-    }
-
-    carregarDados()
-  }, []);
-
+export default function Page() {
   return (
-    <div className="flex flex-col w-[100vw] h-[100vh] items-center overflow-y-auto">
-      {/* Header */}
-      <HeaderHistoricoColaborador tipo={tipo === 'area' ? 'gerenciamento areas' : 'gerenciamento vinculos'} />
-
-      <div className="border-2 w-[40vw] h-[5vh] mt-10 border-laranja-hover cursor-pointer items-center px-4 py-2 flex flex-row rounded-2xl">
-        <input
-          type="text"
-          placeholder="Pesquisar"
-          className="w-full h-full outline-none border-none text-base"
-          value={textoBusca}
-          onChange={(e) => {
-            const novoTexto = e.target.value;
-            setTextoBusca(novoTexto);
-            pesquisar(novoTexto);
-          }}
-        />
-        <Search />
-      </div>
-
-      {/* Conteúdo principal */}
-      <div className="mt-10 flex justify-center">
-        <TabelaAreas areas={areasFiltradas} tipo={tipo} />
-      </div>
-    </div>
+    <Suspense fallback={<div>Carregando...</div>}>
+      <CadastrarAreaVinculo />
+    </Suspense>
   );
 }
