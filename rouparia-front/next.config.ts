84fbc4d96fc65ad8d118674016058d32df63b785
Roberto-Ceschini <robert17ceschini@gmail.com>
2025-04-21 02:43:29 -0300

    Update next.config.ts

--- rouparia-front/next.config.ts
+++ rouparia-front/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // outras configs se tiver
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
